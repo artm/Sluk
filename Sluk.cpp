da0b3decfff349bae78b423d92e19bbcf612e267
Artem Baguinski <artm@v2.nl>
2012-01-10 12:18:13 +0100

    Big bang

---
+++ Sluk.cpp
@@ -0,0 +1,13 @@
+#include <cinder/app/AppBasic.h>
+
+using namespace ci;
+using namespace ci::app;
+
+class Sluk : public AppBasic {
+
+    void setup() { }
+    void update() { }
+    void draw() { }
+};
+
+CINDER_APP_BASIC( Sluk, RendererGl )
